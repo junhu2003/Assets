11ce3dc96e05342c1c4324def47558b1b04f060f
Roger Hu <junhu2003@hotmail.com>
2024-11-27 22:09:12 -0800

    implement functionalities

--- AssetsWeb/src/App.jsx
+++ AssetsWeb/src/App.jsx
@@ -5,12 +5,39 @@ import { AssetsApi } from './assetsApi'
 
 import Accordion from 'react-bootstrap/Accordion';
 import AccordionBody from 'react-bootstrap/esm/AccordionBody';
-import TableContainer from 'react-bootstrap/TabContainer';
-import { FaTrash } from 'react-icons/fa';
 
-function App() {    
+import { FaTrash } from 'react-icons/fa';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import TableContainer from 'react-bootstrap/TabContainer';
+import Modal from 'react-bootstrap/Modal';
+
+
+function App() {
+    const [inputs, setInputs] = useState({ name: '', value: 3000, categoryId: 0 })
+    const [refresh, setRefresh] = useState(false)
+    const [validated, setValidated] = useState(false)
     const [assets, setAssets] = useState([])
     const [categories, setCategories] = useState([])
+    const [show, setShow] = useState(false);
+    const [assetId, setAssetId] = useState(0);
+
+    const handleClose = () => {
+        setAssetId(0);
+        setShow(false);
+    }
+
+    const handleShow = (event, id) => {
+        event.preventDefault();
+
+        if (id) {
+            setAssetId(id);
+            setShow(true);
+        }
+    }
 
     useEffect(() => {
         const debounce = setTimeout(() => {
@@ -19,7 +46,44 @@ function App() {
 
         }, 1000)
         return () => clearTimeout(debounce);
-    }, []);
+    }, [refresh]);
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }));
+    }
+
+    const addItem = () => {
+        if (inputs.name === '' || inputs.value === 0 || inputs.categoryId === 0) {
+            setValidated(false);
+            return;
+        }
+
+        var asset = { name: inputs.name, value: inputs.value, categoryId: inputs.categoryId };
+        AssetsApi.addAsset(asset)
+            .then(() => {
+                setRefresh(!refresh);
+            })
+            .catch(() => {
+                alert('Fail to add new asset!');
+            });
+    }
+
+    const deleteItem = () => {
+
+        if (assetId > 0) {
+            AssetsApi.deleteAssetById(assetId)
+                .then(() => {
+                    setRefresh(!refresh);
+                })
+                .catch(() => {
+                    alert('Fail to delete asset!');
+                });
+        }        
+
+        handleClose();
+    }
 
     const populateAssets = () => {
 
@@ -72,9 +136,9 @@ function App() {
                                                             <div className="col-md-8">{asset.name}</div>
                                                             <div className="col-md-2">${asset.value}</div>
                                                             <div className="col-md-2">
-                                                                <a href="#">
+                                                                <a href="/" onClick={(e) => handleShow(e, asset.id)}>
                                                                     <object>
-                                                                        <FaTrash />
+                                                                        <FaTrash onClick={deleteItem} />
                                                                     </object>
                                                                 </a>
                                                             </div>
@@ -86,9 +150,72 @@ function App() {
                                     </AccordionBody>
                                 </Accordion.Item>
                              </Accordion>;                      
-                  })
+                  })                
+            }
 
-          }
+            <Form noValidate validated={validated}>
+                <Row>
+                    <Col lg="16">
+                        <InputGroup>
+                            <Form.Group as={Col} lg="3" controlId="validationName" className='formGroup'>
+                                <Form.Control
+                                    type="text"
+                                    name="name"
+                                    placeholder="Item Name"
+                                    value={inputs.name}
+                                    onChange={handleChange}
+                                    required                                    
+                                />                                
+                            </Form.Group>
+
+                            <Form.Group as={Col} lg="3" controlId="validationValue" className='formGroup'>
+                                <Form.Control
+                                    type="number"
+                                    name="value"
+                                    placeholder="Value"
+                                    value={inputs.value}
+                                    onChange={handleChange}
+                                    required                                    
+                                />                                
+                            </Form.Group>                            
+
+                            <Form.Group as={Col} lg="4" controlId="validationCategory" className='formGroup'>
+                                <Form.Select
+                                    as="select"
+                                    name="categoryId"
+                                    placeholder="Category"
+                                    value={inputs.categoryId}
+                                    onChange={handleChange}
+                                    required                                    
+                                >
+                                    <option value="0" key="0">Select Category</option>
+                                    {categories && categories.map((c, i) => <option key={i + 1} value={c.id}>{c.name}</option>)}
+                                </Form.Select>
+                            </Form.Group>
+
+                            <Button variant="primary" type="button" onClick={addItem} className='formGroup'>
+                                Add Item
+                            </Button>
+                            
+                        </InputGroup>
+                    </Col>
+                </Row>
+            </Form>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm to delete asset</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure to delete this asset?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={deleteItem}>
+                        Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
     </div>
   )
 }
--- AssetsWeb/src/assetsApi.jsx
+++ AssetsWeb/src/assetsApi.jsx
@@ -26,6 +26,26 @@ export const AssetsApi = {
         return response.json();
     },
     
-    
+    addAsset: async (asset) => {
+        var reqUrl = baseUrl + '/api/Assets';
+        const response = await fetch(reqUrl,
+            {
+                method: 'POST',
+                credentials: 'include',
+                mode: 'cors',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(asset)
+            });
+        return response.json();
+    },
+
+    deleteAssetById: async (id) => {
+        var reqUrl = baseUrl + '/api/Assets/' + id;
+        const response = await fetch(reqUrl, { method: 'DELETE', credentials: 'include', mode: 'cors' });
+        return response.json();
+    },
 
 };
